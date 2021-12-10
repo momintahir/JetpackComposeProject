0a132817753656ee05a8073104fd66393f6d6ac7
Momin Tahir <momintahir7@gmail.com>
2021-12-10 21:34:34 +0500

    Initial commit

---
+++ app/src/main/java/com/example/jetpackcomposeproject/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.jetpackcomposeproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcomposeproject.ui.theme.JetpackComposeProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting(name = "Momin")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeProjectTheme {
+        Greeting("Android")
+    }
+}
